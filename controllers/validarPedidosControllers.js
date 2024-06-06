67c4c114cd13fb99b7b14f7b3d9bc4234c6b521b
Jorge Herrera <jherrera@makita.cl>
2024-06-06 14:06:11 -0400

    refactorizacion

--- controllers/validarPedidosControllers.js
+++ controllers/validarPedidosControllers.js
@@ -11,14 +11,12 @@ const sql = require('mssql');
  */
 async function validarPedidos(req , res){
     try{
-        console.log("req: " , req.body);
         if (!req.body || !req.body.data) {
             logger.error(`Error: los parametros de entrada son incorrectos`);
             return res.status(400).json({ error: `Parámetros faltantes o vacíos` });
         }
 
         const objPedido =  req.body;
-        console.log("objPedido: " , objPedido);
         logger.info(`Iniciamos la funcion validarPedidos`);
 
         // Conectarse a la base de datos 'DTEBdQMakita'
@@ -27,14 +25,11 @@ async function validarPedidos(req , res){
         const consulta = `SELECT * FROM Pedidos where ID_Pedido = '${objPedido.pedido}'`;
         const result = await sql.query(consulta);
         
-        console.log("result: " , result);
-
         logger.info(`Fin  la funcion validarPedidos ${JSON.stringify(result.recordset)}`);
         
         return  res.status(200).json(result.recordset);
      
     }catch (error) {
-        console.log("error: " , error);
         // Manejamos cualquier error ocurrido durante el proceso
         logger.error(`Error en validarPedidos: ${error.message}`);
         res.status(500).json({ error: `Error en el servidor [validar-pedidos-ms] :  ${error.message}`  });
